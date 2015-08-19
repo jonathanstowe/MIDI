6a38a9071b249ae85e6e7df8c539f1934230bd43
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-08-19 08:39:31 +0100

    Actually make some construction

--- lib/MIDI.pm
+++ lib/MIDI.pm
@@ -1,6 +1,10 @@
 use v6;
 
 class MIDI:auth<github:jonathanstowe>:ver<v0.0.1> {
+
+    has Str $!filename;
+    has IO::Handle $!file-handle;
+
     class Track {
         class Event {
         }
@@ -13,17 +17,32 @@ class MIDI:auth<github:jonathanstowe>:ver<v0.0.1> {
     }
 
     class Header {
-        has Str     $.MThd          = "MThd";
+        has Str     $.preamble          = "MThd";
         has uint32  $.header-length = 6;
         has uint16  $.format        = 1;
         has uint16  $.track-chunks  = 0;
         has uint16  $.division      = 96;
+
+        my Str $pattern = "A4 N n n n";
+        my Int $length  = 14;
+
+        has IO::Handle $!file-handle;
+        has Buf $!buf;
+
+        multi submethod BUILD(:$!file-handle!) {
+            $!buf = $!file-handle.read($length);
+            ($!preamble, $!header-length, $!format, $!track-chunks, $!division) = $!buf.unpack($pattern);
+        }
 
     }
 
     has Header $.header;
     has Track @.tracks;
 
+    multi submethod BUILD(:$!filename!) {
+        $!file-handle = $!filename.IO.open(:bin, :r);
+        $!header = Header.new(:$!file-handle);
+    }
 
 =begin comment
 
