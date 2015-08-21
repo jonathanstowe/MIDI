23c09fc8ba89cac51b8921754a0317bd493e08e7
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-08-21 11:08:56 +0100

    Remove that commented out stuff

--- lib/MIDI.pm
+++ lib/MIDI.pm
@@ -43,38 +43,5 @@ class MIDI:auth<github:jonathanstowe>:ver<v0.0.1> {
         $!file-handle = $!filename.IO.open(:bin, :r);
         $!header = Header.new(:$!file-handle);
     }
-
-=begin comment
-
-say MIDI::Header.new;
-#!perl6
-
-use v6;
-
-my $file = open "foo.mid", :bin;
-
-my Buf $buf = Buf.new();
-
-my Str $preamble;
-my Int $header_length;
-my Int $format;
-my Int $n;
-my Int $division;
-
-my Buf $header = $file.read(14);
-
-($preamble, $header_length, $format, $n, $division ) = $header.unpack("A4 N n n n");
-
-say $header;
-
-
-say $preamble;
-say $header_length;
-say $format;
-say $n;
-say $division;
-
-=end comment
-
 }
 # vim: expandtab shiftwidth=4 ft=perl6
